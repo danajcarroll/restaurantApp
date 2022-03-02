5ddf30b40bf875995b6d0fbb022f194782e48623
Dana <danacarroll2000@gmail.com>
2022-03-02 17:06:41 -0500

    Created another restaurant, fixed bug for pages appearing and disappearing

--- js/app.js
+++ js/app.js
@@ -14,7 +14,9 @@ let restaurants = [
         genre: 'Casual Dining',
         diningType: ['Reservations', 'Pick Up'],
         rating: 4,
-        location: '2354 Longlane St, Toronto, ON, M4B 124',
+        street: '2354 Longlane St',
+        city: 'Toronto, ON',
+        postalCode: 'M4B 124',
         miniBio: 'Enjoy a truly authentic Italian dining experience at Five Vines. With ingredients sourced from Italy, such as winemakers, meat and artisinal cheese producers and so much more. ',
         hours: {
             mon: {
@@ -130,6 +132,138 @@ let restaurants = [
             ]
         } // End of Menu
 
+    }, // End of Restaurant
+    { // Start of restaurant
+        id: 2,
+        name: 'Hearth Bistro',
+        logo: './images/logo-hearth.png',
+        images: [
+            '',
+            '',
+            ''
+        ],
+        genre: 'Fast Casual Dining',
+        diningType: ['Reservations', 'Pick Up'],
+        rating: 4,
+        location: '1468 Brummer St',
+        street: '1468 Brummer St',
+        city: 'Toronto, ON',
+        postalCode: 'L8B 248',
+        miniBio: 'A symbol of warmth and welcome, Hearth and Stone Bistro is where you come to eat when you want eat out yet feel at home. ',
+        hours: {
+            mon: {
+                open: '11am',
+                close: '10pm'
+            },
+            tues: {
+                open: '11am',
+                close: '10pm'
+            },
+            wed: {
+                open: '11am',
+                close: '10pm'
+            },
+            thur: {
+                open: '11am',
+                close: '10pm'
+            },
+            fri: {
+                open: '12pm',
+                close: '11pm'
+            },
+            sat: {
+                open: '12pm',
+                close: '11pm'
+            },
+            sun: {
+                open: '12pm',
+                close: '11pm'
+            }
+        }, 
+        menu: {
+            starters: [
+                {
+                    name: 'Four Cheese Spinach Dip',
+                    desc: 'Made in-house creamy blend of four cheeses, spinach, red pepper and onion. Served with your choice of baked pita chips or fried pita chips',
+                    price: 14.99
+                },
+                {
+                    name: 'Buffalo Chicken Potato Skins',
+                    desc: 'Hand-cut potato wedges topped with spiced chicken tossed in our creamy buffalo wing sauce and our three cheese blend',
+                    price: 14.99
+                },
+                {
+                    name: 'Quesadilla',
+                    desc: 'Our three cheese blend, tomato, onions and jalapeño. Served with salsa and sour cream.',
+                    price: 13.49
+                },
+                {
+                    name: 'House Garlic Cheese Toast',
+                    desc: 'A gooey six cheese blend freshly-baked on two Ace Bakery French Loaf slices with garlic, topped with a sprinkle of Sriracha lime seasoning',
+                    price: 9.99
+                },
+                {
+                    name: 'Truffle Fries and Dip',
+                    desc: 'Our house russet fries topped with truffle oil, parmesan cheese, and parsley, with roasted garlic dip on the side',
+                    price: 18
+                }
+            ],
+            mains: [
+                {
+                    name: 'Classic Sirloin',
+                    desc: 'Fresh 8oz. sirloin grilled to your liking and brushed with garlic butter. Served with your choice of side and freshly steamed veggies',
+                    price: 25.99
+                },
+                {
+                    name: 'Lemon Pepper Salmon',
+                    desc: 'Atlantic salmon oven baked with a lemon pepper seasoning and paired with a basil pesto aioli. Served with your choice of side and freshly steamed veggies',
+                    price: 23.99
+                },
+                {
+                    name: 'Chicken Parm',
+                    desc: 'A hand breaded chicken breast with Parmesan cheese, topped with tomato sauce and mozzarella cheese. Served over spaghetti with your choice of tomato sauce or Alfredo sauce',
+                    price: 19.79
+                },
+                {
+                    name: 'Caesar Salad',
+                    desc: 'Chopped romaine lettuce tossed in creamy Caesar dressing and topped with double-smoked bacon, croutons and Parmesan cheese',
+                    price: 13.49
+                },
+                {
+                    name: 'Sonoma Chicken Salad',
+                    desc: 'Grilled chicken breast on fresh California greens topped with red pepper, tomato, crumbled feta cheese, raisins, croutons, mixed seeds and nuts and finished with our honey citrus dressing',
+                    price: 17.99
+                }
+            ],
+            desserts: [
+                {
+                    name: 'White Chocolate Cheesecake',
+                    desc: 'A dark chocolate cookie crumble crust filled with thick white chocolate cheesecake, with your choice of chocolate or caramel sauce',
+                    price: 7.99
+                },
+                {
+                    name: 'Funnel Cake Fries',
+                    desc: 'Golden fried strips of funnel cake topped with cinnamon sugar. Served warm with French Vanilla ice cream and choice of chocolate sauce or caramel sauce',
+                    price: 7.99
+                },
+                {
+                    name: 'Messy Cookie Sandwich',
+                    desc: 'French vanilla ice cream sandwiched between two giant freshly baked warm chocolate chip cookies. Topped with chocolate and caramel sauce',
+                    price: 8.49
+                },
+                {
+                    name: 'Chocolate Peanut Butter Tart',
+                    desc: 'Tart topped with chocolate sauce, roasted peanuts and coconut whipped cream',
+                    price: 9.98
+                },
+                {
+                    name: 'Lemon Blueberry Tart',
+                    desc: 'House-made lemon blueberry tart with lemon streusel, whipped cream and fresh blueberries',
+                    price: 9
+                }
+            ]
+        } // End of Menu
+
     } // End of Restaurant
 ];
 
@@ -139,6 +273,8 @@ const restMenuPage = document.getElementById('restMenu');
 const restaurantList = document.getElementById('restList');
 const restHomeContainer = document.getElementById('restaurantHomeContainer');
 const homeButton = document.getElementById('homeButton');
+const viewMenuButton = document.getElementById('viewMenuButton');
+const restHomeButton = document.getElementById('restHomeButton');
 
 
 // Display Restaurants on Splash Page
@@ -147,9 +283,9 @@ function displayRestaurantThumbnails() {
         return `
         <li class="restThumbnails" id="${rest.id}">
             <div class="restThumbImage">${rest.logo}</div>
-            <div class="restThumbInfoContainer">
+            <div class="restThumbInfo">
                 <h3 class="restThumbName">${rest.name}</h3>
-                <p class="restThumbGenre">${rest.diningType}</p>
+                <p class="restThumbCity">${rest.city}</p>
                 <p class="restThumbRating">${rest.rating}</p>
             </div>
         </li>`
@@ -176,7 +312,16 @@ function displayRestaurantHome(restID) {
 homeButton.addEventListener('click', function() {
     restHomePage.classList.remove('activePage');
     splashPage.classList.add('activePage');
-})
+    splashPage.style.display = 'grid';
+});
+viewMenuButton.addEventListener('click', function() {
+    restHomePage.classList.remove('activePage');
+    restMenuPage.classList.add('activePage');
+});
+restHomeButton.addEventListener('click', function() {
+    restMenuPage.classList.remove('activePage');
+    restHomePage.classList.add('activePage');
+});
 
 
 
@@ -192,6 +337,7 @@ window.addEventListener('DOMContentLoaded', function() {
     restaurantThumbnails.forEach(rest => {
         rest.addEventListener('click', function() {
             splashPage.classList.remove('activePage');
+            splashPage.style.display = 'none';
             restHomePage.classList.add('activePage');
             let restaurantHomeHTML = displayRestaurantHome(`${rest.id}`);
             restHomeContainer.innerHTML = restaurantHomeHTML;
